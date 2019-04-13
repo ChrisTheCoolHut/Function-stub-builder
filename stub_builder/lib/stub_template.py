0d9548d612656bfa45a5efebccdef9a6feba1078
Christopher Roberts <crober27@masonlive.gmu.edu>
2019-04-13 14:37:28 -0700

    support for uclibc

--- stub_builder/lib/stub_template.py
+++ stub_builder/lib/stub_template.py
@@ -42,5 +42,29 @@ int __libc_start_main(
     /* ... and call it with our custom main function */
     return orig(main_hook, argc, argv, init, fini, rtld_fini, stack_end);
 }}
+
+//uClibc_main
+/*
+ * Wrapper for __uClibc__main() that replaces the real main
+ * function with our hooked version.
+ */
+int __uClibc_main(
+    int (*main)(int, char **, char **),
+    int argc,
+    char **argv,
+    int (*init)(int, char **, char **),
+    void (*fini)(void),
+    void (*rtld_fini)(void),
+    void *stack_end)
+{{
+    /* Save the real main function address */
+    main_orig = main;
+
+    /* Find the real __uClibc_main()... */
+    typeof(&__uClibc_main) orig = dlsym(RTLD_NEXT, "__uClibc_main");
+
+    /* ... and call it with our custom main function */
+    return orig(main_hook, argc, argv, init, fini, rtld_fini, stack_end);
+}}
 '''
 
